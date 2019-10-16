a4dc64ab1dee787073fa21fe7d39aee2eda0f95a
peter <peterkanyora341@gmail.com>
2019-10-16 04:24:45 +0300

    initial commit

---
+++ admin/home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>|| MZALENDO SYSTEM ||</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="icon" type="image/png" href="../images/icons/favicon.png"/>
+<link href="../default.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style1 {
+	color: #F8941D;
+	font-weight: bold;
+	font-size: 14px;
+}
+-->
+</style>
+</head>
+<body>
+<div id="header">
+</div>
+<div id="menu" align="center">
+	<ul>
+		<li><a href="../index.php">|  Home  |</a></li>
+		<li><a href="../user_login.php">|  Voting  |</a></li>
+		<li><a href="../result.php">|  Result  |</a></li>
+		<li><a href="index.php">|  Backend  |</a></li>
+		<li><a href="../contact.php">|  Contact Us  |</a></li>
+        <li><a href="../index.php">|  Logout  |</a></li>
+	</ul>
+</div>
+
+<p align="center" class="style1"> Mzalendo E-Voting System Admin Control Panel</p>
+<div id="nav" align="center">
+	<ul>
+		<li><a href="position.php">|Position Info|</a></li>
+		<li><a href="candidate.php">|Candidate Info|</a></li>
+		<li><a href="students.php">|Voters Info|</a></li>
+		<li><a href="tally.php">|Vote Analisys|</a></li>
+        
+	</ul>
+</div>
+
+<p>&nbsp;</p>
+<div id="footer">
+	<p>Copyright &copy; 2019 Designed by <a href="http://www.charlesmbuvi.com" class="link1">Charles Mbuvi</a></p>
+</div>
+</body>
+</html>
---
+++ admin/students_control_panel.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<title>Voters Control Panel</title>
+<link rel="icon" type="image/png" href="../images/icons/favicon.png"/>
+<meta name="generator" content="dbQwikSite Ecommerce"><meta name="dbQwikSitePE" content="QSFREEPE">
+<style type="text/css">
+<!--
+.fontnormal {
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 12px;
+}
+.fontheader {
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 20px;
+}
+-->
+  </style>
+</head>
+<body>
+<p align="center" class="fontheader"><strong>Voters
+  Control Panel</strong></p>
+<div>
+<div id="nav" align="center">
+	<ul>
+		<a href="position.php">|Position Info|</a>
+		<a href="candidate.php">|Candidate Info|</a>
+		<a href="students.php">|Voters Info|</a>
+		<a href="tally.php">|Vote Analisys|</a>
+        
+	</ul>
+</div>
+<table qwsid="535.TExpHtml.sitegroupcontrolpanel.htmlsite  BgColor="#D4D4D4" cellspacing="1" cellpadding="2" width="320" border="0" align="center" BorderColor="#000020">
+<tbody>
+        <tr  BgColor="#FFFFE1">
+          <td height="40" class="fontnormal">&nbsp;&nbsp;&nbsp;&nbsp;Voters Data</td>
+          <td height="40" align="center" valign="middle"><a href="./students.php"><img src="./images/bt_data.gif" border="0"  onerror="this.onerror=null;this.src='./images/qs_nopicture.gif';" ></td>
+        </tr>
+        <tr  BgColor="#FFFFE1">
+          <td height="40" class="fontnormal">&nbsp;&nbsp;&nbsp;&nbsp;Find Voter</td>
+          <td height="40" align="center" valign="middle"><a href="./students_search.php"><img src="./images/bt_search.gif" border="0"  onerror="this.onerror=null;this.src='./images/qs_nopicture.gif';" ></td>
+        </tr>
+        <tr  BgColor="#FFFFE1">
+          <td height="40" class="fontnormal">&nbsp;&nbsp;&nbsp;&nbsp;Add Voter</td>
+          <td height="40" align="center" valign="middle"><a href="./students_add.php"><img src="./images/bt_qsadd_new.gif" border="0"  onerror="this.onerror=null;this.src='./images/qs_nopicture.gif';" ></td>
+        </tr>
+        
+</tbody>
+</table>
+</div>
+</body>
+</html>
